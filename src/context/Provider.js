b3977063b99a05e734e7742314e46dce5bc7ffed
Henrik Santos <36730816+HenrikSantos@users.noreply.github.com>
2022-10-18 18:27:45 -0300

    Initial commit

---
+++ src/context/Provider.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import MyContext from "./myContext";
+
+const INITIAL_STATE = {
+  id: 1,
+  title: "Título do email",
+  status: 0
+};
+
+function Provider({ children }) {
+  const [email, setEmail] = useState(INITIAL_STATE);
+
+  const value = {
+    email,
+    setEmail
+  };
+  return <MyContext.Provider value={value}>{children}</MyContext.Provider>;
+}
+
+export default Provider;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Provider } from "./context/Provider";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
